247c1780a660a078be08586350a1ba9ab695aab0
Vitor <vitorh.7@outlook.com>
2024-10-24 20:34:29 -0300

    Adicionando o projeto memoteca

---
+++ src/app/componentes/pensamentos/excluir-pensamento/excluir-pensamento.component.ts
@@ -0,0 +1,44 @@
+import { PensamentoService } from './../pensamento.service';
+import { Component, OnInit } from '@angular/core';
+import { Pensamento } from '../pensamento/pensamento';
+import { ActivatedRoute, Router } from '@angular/router';
+
+@Component({
+  selector: 'app-excluir-pensamento',
+  templateUrl: './excluir-pensamento.component.html',
+  styleUrls: ['./excluir-pensamento.component.css']
+})
+export class ExcluirPensamentoComponent implements OnInit {
+
+  pensamento: Pensamento = {
+    id: '',
+    conteudo: '',
+    autoria: '',
+    modelo: ''
+  }
+  constructor(
+    private service: PensamentoService,
+    private router: Router,
+    private route: ActivatedRoute,
+
+  ) { }
+
+  ngOnInit(): void {
+    const id = this.route.snapshot.paramMap.get('id');
+    this.service.buscarPorId(id!).subscribe((pensamento) =>{
+      this.pensamento = pensamento
+    })
+  }
+
+  excluirPensamento(){
+    if(this.pensamento.id){
+      this.service.excluir(this.pensamento.id).subscribe(() => {
+        this.router.navigate(['/listarPensamento']);
+      })
+    }
+  }
+
+  cancelar(){
+    this.router.navigate(['/listarPensamento']);
+  }
+}
